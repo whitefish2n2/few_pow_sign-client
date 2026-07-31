99c07a642531815f9180e27d77a06867a27876e8
hasaeng <whitefish822@gmail.com>
2026-07-31 20:32:18 +0900

    HUD 매니저

---
+++ Assets/Codes/InGame/InGameHudUIManager.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+namespace Codes.InGame
+{
+    public class InGameHudUIManager : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI hpText;
+        [SerializeField] private TextMeshProUGUI ammoText;
+        [SerializeField] private TextMeshProUGUI myTeamScoreText;
+        [SerializeField] private TextMeshProUGUI enemyTeamScoreText;
+
+        private void Update()
+        {
+            if (!InGameLogicStatic.IsInitialized) return;
+
+            hpText.text = InGameLogicStatic.Instance.GetMyHpText();
+            ammoText.text = InGameLogicStatic.Instance.GetMyAmmoText();
+            myTeamScoreText.text = InGameLogicStatic.Instance.GetMyTeamScore().ToString();
+            enemyTeamScoreText.text = InGameLogicStatic.Instance.GetBestEnemyTeamScore().ToString();
+        }
+    }
+}
